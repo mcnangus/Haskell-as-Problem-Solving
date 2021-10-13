8973f7d81eb0fc89ac81ecafa11547339bc34db8
mcnangus <69413915+mcnangus@users.noreply.github.com>
2021-10-13 17:17:23 +1100

    Delete src directory

--- src/Warmup.hs
+++
@@ -1,9 +0,0 @@
---
--- COMP1100/1130, Semester 1, 2018
---
-
-module Main where
-import CodeWorld
-
-main :: IO ()
-main = drawingOf ((translated 3 3 (solidCircle 2)) & (translated 4 4 (coloured red (solidRectangle (-3) 6))))
