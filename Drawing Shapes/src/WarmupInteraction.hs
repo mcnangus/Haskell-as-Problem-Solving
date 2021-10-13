0262a3940cbf4471d05e882d52d082b0dd422b8d
mcnangus <69413915+mcnangus@users.noreply.github.com>
2021-10-13 17:17:08 +1100

    Add files via upload

---
+++ Drawing Shapes/src/WarmupInteraction.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+import CodeWorld
+
+type Coord = (Double, Double)
+
+initialCoord :: Coord
+initialCoord = (0,0)
+
+main :: IO()
+main = interactionOf initialCoord handleTime handleEvent drawState
+
+handleTime :: Double -> Coord -> Coord
+handleTime _ c = c
+
+-- |
+-- >>> :set -XOverloadedStrings
+-- >>> handleEvent (KeyPress "Up") (0,0)
+-- (0.0,1.0)
+
+handleEvent :: Event -> Coord -> Coord
+handleEvent e (x,y) = case e of
+  KeyPress "Up" -> (x,y+1)
+  KeyPress "Down" -> (x,y-1)
+  KeyPress "Left" -> (x-1,y)
+  KeyPress "Right" -> (x+1,y)
+  _ -> (x,y)
+
+drawState :: Coord -> Picture
+drawState (x,y) = translated x y (solidCircle 1)
+
